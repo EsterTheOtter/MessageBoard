cf60186af561ff6ea1fb70eb57fabb051b2fe8f3
Ester The Otter <83263521+EsterTheOtter@users.noreply.github.com>
2023-11-08 12:17:58 -0500

    Create Profile class
    -Quickly made Profile class, should be responsible for managing user profile settings via iomanip

---
+++ ProfileClass.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+
+#include "ProfileClass.hpp"
+
+
+//operators
+//functions
+Profile::Profile() {
+    pName;
+}
+//accessors
+    //getters
+    //setters
+//modifiers
---
+++ ProfileClass.hpp
@@ -0,0 +1,20 @@
+#ifndef ProfileClass_hpp
+#define ProfileClass_hpp
+
+#include <string>
+
+
+class Profile {
+    private:
+    std::string pName;
+    public:
+    //operators
+    //functions
+    Profile();
+    //accessors
+        //getters
+        //setters
+    //modifiers
+};
+
+#endif
