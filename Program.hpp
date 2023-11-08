13adf8e4249bac2db12e1003584de6cd55ceaa5a
Ester The Otter <83263521+EsterTheOtter@users.noreply.github.com>
2023-11-08 10:42:48 -0500

    Create main repository and files
    -Create the main.cpp
    -Create the Program.cpp
    -Create the Program.hpp

---
+++ Program.hpp
@@ -0,0 +1,16 @@
+#ifndef Program_hpp
+#define Program_hpp
+
+
+class Program {
+    private:
+    public:
+    //operators
+    //functions
+    //accessors
+        //getters
+        //setters
+    //modifiers
+};
+
+#endif
